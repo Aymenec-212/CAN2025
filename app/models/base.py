b40cfbc988e4fd200f213e9f22e44132877cbe6c
AymenEc <91324849+AymenE19@users.noreply.github.com>
2025-12-23 19:49:44 +0100

    fix(ci): fix mypy issues 2

--- app/models/base.py
+++ app/models/base.py
@@ -2,7 +2,6 @@
 from typing import Any
 
 from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import DeclarativeBase
 
 # Naming convention for constraints to keep Alembic happy
